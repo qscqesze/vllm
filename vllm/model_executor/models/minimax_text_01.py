2d870a10ace94ed68158c003aea95d2a916e6a41
qingjun <qingjun@minimaxi.com>
2025-04-07 11:45:39 +0800

    fix code

--- vllm/model_executor/models/minimax_text_01.py
+++ vllm/model_executor/models/minimax_text_01.py
@@ -38,7 +38,7 @@ from vllm.model_executor.layers.vocab_parallel_embedding import (
     DEFAULT_VOCAB_PADDING_SIZE, ParallelLMHead, VocabParallelEmbedding)
 from vllm.model_executor.model_loader.weight_utils import default_weight_loader
 from vllm.model_executor.models.utils import (
-    maybe_prefix, AutoWeightsLoader, WeightsMapper, weight_loader_with_alias)
+    maybe_prefix, AutoWeightsLoader, WeightsMapper)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.sequence import IntermediateTensors
 
